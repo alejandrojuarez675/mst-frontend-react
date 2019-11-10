6c4906aa20e91412e8432040caace478677790cb
alejandro <alejandrojuarez675@gmail.com>
2019-11-10 12:44:16 -0300

    saco seccion mapas y upgrade

--- src/components/share/Sidebar/Sidebar.jsx
+++ src/components/share/Sidebar/Sidebar.jsx
@@ -1,10 +1,7 @@
-
 import logo from "assets/img/reactlogo.png";
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import AdminNavbarLinks from "../Navbars/AdminNavbarLinks";
-
-
 
 class Sidebar extends Component {
   constructor(props) {
@@ -34,26 +31,16 @@ class Sidebar extends Component {
         data-color={this.props.color}
         data-image={this.props.image}
       >
-          {this.props.hasImage ? (
-            <div className="sidebar-background" style={sidebarBackground} />
-          ) : (
-            null
-          )}
+        {this.props.hasImage ? (
+          <div className="sidebar-background" style={sidebarBackground} />
+        ) : null}
         <div className="logo">
-          <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
-            className="simple-text logo-mini"
-          >
+          <a className="simple-text logo-mini">
             <div className="logo-img">
               <img src={logo} alt="logo_image" />
             </div>
           </a>
-          <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
-            className="simple-text logo-normal"
-          >
-            mst-frontend-react
-          </a>
+          <a className="simple-text logo-normal">tecso</a>
         </div>
         <div className="sidebar-wrapper">
           <ul className="nav">
@@ -63,9 +50,7 @@ class Sidebar extends Component {
                 return (
                   <li
                     className={
-                      prop.upgrade
-                        ? "active active-pro"
-                        : this.activeRoute(prop.layout + prop.path)
+                        this.activeRoute(prop.layout + prop.path)
                     }
                     key={key}
                   >
